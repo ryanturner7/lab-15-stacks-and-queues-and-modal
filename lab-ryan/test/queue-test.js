3c7730a172d0048cb89d8eb0002956917384f7bf
Ryan Turner <ryanthepit@gmail.com>
2017-07-03 01:07:19 -0700

    added queue test

--- lab-ryan/test/queue-test.js
+++ lab-ryan/test/queue-test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Queue = require('../queue.js');
+const Stack = require('../stack.js')
 const expect = require('expect');
 
 describe('Testing Queue methods', () => {
@@ -22,6 +23,25 @@ describe('Testing Queue methods', () => {
       let myNameEnqueued = myQueue.enqueue({name: 'Ryan'});
       let myNameDequeued = myNameEnqueued.dequeue();
       expect(myNameDequeued).toEqual({name: 'Ryan'});
+      let myNamePopped = myNamePushed.pop();
+      expect(JSON.stringify(myNamePopped))
+      .toEqual(JSON.stringify({ name: 'Ryan' }));
+      expect(JSON.stringify(myStack)).toEqual(JSON.stringify({ 0: { name: 'Hobart' }, length:1 }));
+    });
+  });
+  describe('Testing Stack and pop methods.', () => {
+    it('should push item onto top of stack', () => {
+      let myStack = new Stack();
+      let myNamePushed = myStack.push({name: 'Ryan'});
+      expect(JSON.stringify(myNamePushed))
+      .toEqual(JSON.stringify({ 0: { name: 'Ryan' }, length: 1 }));
+    });
+  });
+  describe('Testing Stack pop method.', () => {
+    it('should pop last item off of stack', () => {
+      let myStack = new Stack();
+      myStack.push({name: 'Hobart'});
+      let myNamePushed = myStack.push({name: 'Ryan'});
       let myNamePopped = myNamePushed.pop();
       expect(JSON.stringify(myNamePopped))
       .toEqual(JSON.stringify({ name: 'Ryan' }));
